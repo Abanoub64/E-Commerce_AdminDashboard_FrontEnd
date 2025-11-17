3cc12be2af0365d2bab54fb3bd13a3dc55f46e7a
Abanoub Milad <abanoubmilad64@gmail.com>
2025-11-17 03:24:24 +0200

    Remove unused Clerk middleware and clean up Navbar imports

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,4 @@
 "use client";
-import { useClerk } from "@clerk/nextjs";
 import { Search, ShoppingCart } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
